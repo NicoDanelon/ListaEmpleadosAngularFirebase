c17a20e03677114e1b15547988d62672ffbbab96
NicoDanelon <54380841+NicoDanelon@users.noreply.github.com>
2021-04-05 23:58:12 -0300

    lista empleados leo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDd0dXtZtRPAyBpkSlbCWvbzUfakGFMxpY",
+    authDomain: "empleados-43b63.firebaseapp.com",
+    projectId: "empleados-43b63",
+    storageBucket: "empleados-43b63.appspot.com",
+    messagingSenderId: "844251096548",
+    appId: "1:844251096548:web:fe50a334a0beec23c8d3f1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDd0dXtZtRPAyBpkSlbCWvbzUfakGFMxpY",
+    authDomain: "empleados-43b63.firebaseapp.com",
+    projectId: "empleados-43b63",
+    storageBucket: "empleados-43b63.appspot.com",
+    messagingSenderId: "844251096548",
+    appId: "1:844251096548:web:fe50a334a0beec23c8d3f1"
+  }
 };
 
 /*
